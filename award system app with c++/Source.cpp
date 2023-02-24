1eef43e25e3201cc3cd48a2cbba443ba8ede4da8
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-02-24 23:14:59 +0200

    Add files via upload

---
+++ award system app with c++/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << "enter the number you received \n";
+	cin >> num;
+	switch (num)
+	{
+	case 100:
+	case 200:
+		cout << "congratz for The iphone ";
+		break;
+	case 300:
+		cout << "congratz for the ipad ";
+		break;
+	default:
+		cout << "No award for this number ";
+		break;
+	}
+}
